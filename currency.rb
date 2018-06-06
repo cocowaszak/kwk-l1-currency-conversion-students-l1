26ff4947ce6772ddbd3f4d7d70e04c458189e1c7
cocowaszak <colettewaszak@gmail.com>
2018-06-06 16:57:11 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,39 @@
-# Write your cod here
+def usd_to_eur(dollar)
+  puts dollar*0.85
+end 
+usd_to_eur(20)
+
+def eur_to_usd(euro)
+  puts euro*1.17
+end 
+eur_to_usd(20)
+
+def usd_to_jpy(dollar)
+  puts dollar*109.82
+end 
+usd_to_jpy(20)
+
+def usd_to_gbp(dollar)
+  puts dollar*0.75
+end
+usd_to_gbp(20)
+
+def usd_to_aud(dollar)
+  puts dollar*1.31
+end 
+usd_to_aud(20)
+
+def jpy_to_usd(dollar)
+  puts dollar*0.0091
+end 
+jpy_to_usd(20)
+
+def gbp_to_usd(dollar)
+  puts dollar*1.34
+end
+gbp_to_usd(20)
+
+def aud_to_usd(dollar)
+  puts dollar*0.76
+end
+aud_to_usd(20)
